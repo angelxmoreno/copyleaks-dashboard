85394455d5d394cd51315594022f6898d0dc452c
Angel S. Moreno <angelxmoreno@gmail.com>
2018-05-26 19:55:15 -0400

    Added webpack

--- src/main/index.js
+++ src/main/index.js
@@ -3,13 +3,14 @@ const {app, BrowserWindow} = require('electron');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
+const index_file = __dirname + '/../../html/index.html';
 
 function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({width: 800, height: 600});
 
     // and load the index.html of the app.
-    win.loadFile('html/index.html');
+    win.loadFile(index_file);
 
     // Open the DevTools.
     win.webContents.openDevTools();
